24c62cc2347a9b900123245569a44501a645fb5b
sumanthpalliboina <palleboinasumanth@gmail.com>
2024-04-03 12:10:34 +0530

    UIStackView and AutoLayout constraints

--- AutoLayoutAndSizeClasses/ViewController.swift
+++ AutoLayoutAndSizeClasses/ViewController.swift
@@ -9,9 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myView: UIView!
+    
+    @IBOutlet weak var myView2: UIView!
+    
+    @IBOutlet weak var myLabel1: UILabel!
+    
+    @IBOutlet weak var myLabel2: UILabel!
+    
+    @IBOutlet weak var vStack: UIStackView!
+    
+    @IBOutlet weak var hStack: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myView.backgroundColor = UIColor.systemGray
+        myView2.backgroundColor = UIColor.systemGray
+        
+        myLabel1.text = "Description : my label text"
+        myLabel1.numberOfLines = 2
+        myLabel2.text = "Description : my label text"
+        myLabel2.numberOfLines = 2
+        
+        //hStack.distribution = .fillEqually
+        //hStack.spacing = 5
+        vStack.alignment = .fill
+        vStack.distribution = .fillEqually
+        vStack.spacing = 4
+        
+        let btn = UIButton(configuration: .filled(),primaryAction: UIAction(title:"Press Here",handler: { action in
+            print("Hello")
+        }))
+        
+        vStack.addArrangedSubview(btn)
+        
     }
 
 
